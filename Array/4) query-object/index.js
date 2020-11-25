9fa9479362b7365c6037cdfb0efdc2bd3d0fdd15
petrukhinilya <petrukhinilya.exceed@gmail.com>
2020-11-25 17:53:54 +0300

    chngedtosubmit

--- Array/4) query-object/index.js
+++ Array/4) query-object/index.js
@@ -1,24 +1,33 @@
 function queryToObject(query) {
-  switch(query){
-  case 'null': return null
-  case 'undefined': return undefined
-  case 'true': return true
-  case 'false': return false
-}
+//   switch(query){
+//   case 'null': return null
+//   case 'undefined': return undefined
+//   case 'true': return true
+//   case 'false': return false
+// }
 
-let num = Number(query) 
-if(!isNaN(num)&& str.length >0){
-  return num
-}
-if(typeof(query) === 'string'){
-  return query
-} 
+// let num = Number(query) 
+// if(!isNaN(num)&& str.length >0){
+//   return num
+// }
+// if(typeof(query) === 'string'){
+//   return query
+// } 
 
-  let arr = query.split('')
-  let obj = {}
-  for (let i = 0; i < arr.length; i++)
-    obj[i] = arr[i];
-  return obj;
+  let str = query
+  if(str.length==''){
+    return {}
+  }
+  str = str.replace('?','')
+  if(str.length==''){
+    return {}
+  }
+  
+  str = str.replace(/=/g,'":')
+  str = str.replace(/&/g,',')
+  
+  return str
+
 };
 
 window.queryToObject = queryToObject;
--- Test Task/index.js
+++ Test Task/index.js
@@ -1,23 +1,53 @@
-function sendForm(){
-    // let formTo = document.getElementById('sendTo')
-    let formFrom = document.getElementById('getTo')
-    
+
+// let div = document.createElement('div')
+// let newForm = document.createElement('input')
+// let newButton = document.createElement('input') 
+// newForm.type='text'
+// newButton.type='button'
+// newButton.value = 'отправить' 
+// document.body.append(div)
+// div.insertAdjacentElement('afterbegin',newForm)
+// div.insertAdjacentElement('beforeend',newButton)
+
+
+// newButton.addEventListener('click',function(){ 
+//     // console.log(document.body.lastElementChild.lastChild) 
+ 
+//     document.body.append(div)
+//     div.insertAdjacentElement('afterbegin',newForm)
+//     div.insertAdjacentElement('beforeend',newButton)
+// })
+
+
+
+
+
+let formTo = document.getElementById('sendTo')
+let form = document.getElementById('form')
+
+formTo.addEventListener('click',function(){
+
+
     let newForm = document.createElement('input')
+
     newForm.type='text'
-    newForm.id ='newform'
+    newForm.id = 'button'
+
+    let formFrom = document.getElementById('getTo')
     newForm.value = formFrom.value
-    if(formFrom == 555){
-        return value = '---'
+
+
+    if(formFrom.value== '555'){
+        return formFrom.value = '---'
     }
+
     document.body.append(newForm)
-    console.log('lf')
+
+    // let number = document.querySelectorAll('#button')
+    // if(number.length % 2 == 0){
+    //     newForm.value.toUpperCase()
+    // }else{newForm.value.toLowerCase()}
+
+})
 
 
-}
-// function getForm(){
-//     let formTo = document.getElementById('sendTo')
-//     let value = formTo.value 
-//     if(value =='555'){
-//         return value = '---'
-//     }
-// }
